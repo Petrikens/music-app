16ab9b7e258dae9364050632221c62cb2da6c6df
Petrikens <ppvs96@mail.ru>
2023-05-22 09:29:08 +0300

    added routing

--- src/router/index.js
+++ src/router/index.js
@@ -2,25 +2,59 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
 import ManageView from '@/views/ManageView.vue'
+import useUserStore from '@/stores/user'
+import { useResetForm } from 'vee-validate'
 
 const routes = [
   {
+    name: 'home',
     path: '/',
     component: HomeView
   },
   {
+    name: 'about',
     path: '/about',
     component: AboutView
   },
   {
+    name: 'manage',
+    // alias: '/manage',
+    path: '/manage-music',
+    component: ManageView,
+    meta: {
+      requiredAuth: true
+    }
+  },
+  {
     path: '/manage',
-    component: ManageView
+    redirect: { name: 'manage' }
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
+})
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta.requiredAuth) {
+    next()
+    return
+  }
+
+  const store = useUserStore()
+
+  if (store.userLoggedIn) {
+    next()
+    return
+  } else {
+    next({ home: 'home' })
+  }
 })
 
 export default router
